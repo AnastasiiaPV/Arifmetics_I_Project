bb605a6dd80105aa127baef872a9cab9750ffc87
Anastasiia Potapenko <oberonapa@gmail.como>
2021-10-25 17:52:38 -0400

    xPath

---
+++ src/main/java/xPath_16.java
@@ -0,0 +1,9 @@
+public class xPath_16 {
+
+    ////a[contains(@id,'code-tab')]
+    ////a[contains(@id,'issues-tab')]
+    ////a[contains(@id,'pull')]
+    ////summary[contains(@class,'rounded')]
+    //button[@class='js-toggler-target btn-with-count btn-sm btn' and contains(@data-hydro-click ,'target":"STAR_BUTTON') ];
+    ////button[contains(@data-hydro-click ,'target":"FORK_BUTTON') ]
+}

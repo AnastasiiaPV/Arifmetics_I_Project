784461cae60ed918fc26ab93184cadb87a087eaa
Anastasiia Potapenko <oberonapa@gmail.como>
2021-10-29 12:08:57 -0400

    Replace 0 to o

---
+++ src/main/java/Change0toO.java
@@ -0,0 +1,8 @@
+public class Change0toO {
+    //Написать метод, который принимает строку и заменяет все цифры 0 на букву “o”.
+    //
+
+    public static String getZeroToO(String string) {
+        return string.replace('0', 'o');
+    }
+}

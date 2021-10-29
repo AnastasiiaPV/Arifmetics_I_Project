a18a1d4c28fb716986d9496ac6ac198f20ab51ac
Anastasiia Potapenko <oberonapa@gmail.como>
2021-10-29 12:37:14 -0400

    Delete space in String and convert String to Lover case

---
+++ src/main/java/DeleteSpace_ToLoverCase.java
@@ -0,0 +1,18 @@
+public class DeleteSpace_ToLoverCase {
+    //Написать метод, который удалит все пробелы из входящей строки,
+    // и заменит все большие буквы на маленькие.
+    //
+    public static String deleteSpaceToLoverCase(String string){
+        string = string.toLowerCase();
+        String newString = "";
+        for (int i = 0; i < string.length(); i++) {
+            char letter = string.charAt(i);
+            if(letter!=' '){
+                newString = newString.concat(String.valueOf(letter));
+            }
+        }
+        System.out.println(newString);
+        return newString;
+    }
+
+}

ca9eb7ec4ae540d99576a40f800ff1a9d5de29a3
Anastasiia Potapenko <oberonapa@gmail.como>
2021-11-02 19:15:26 -0400

    Tasks for HW19_20

---
+++ src/main/java/HW_19_20/General_Tasks.java
@@ -0,0 +1,103 @@
+package HW_19_20;
+
+public class General_Tasks {
+
+    //1.Напишите метод, который выводит строковый параметр  n раз и показывает количество символов в новой строке.
+
+
+    //2.Напишите метод, который принимает строковый параметр и возвращает слово, которое состоит из последних
+    // трех и первых трех букв слова-параметра, если длина строки больше 5. Иначе метод возвращает слово,
+    // состоящее из средней/средних буквы/букв слова-параметра, повторенных столько раз, какова длина строки.
+    //
+    //Пример:
+    //method(“QAForEveryone”) -> “oneQAF”
+    //method(“Java”) -> “avavavav”
+    //method(“A”) -> “A”
+
+
+    //3.Напишите метод, который в строке находит слова минимальной и максимальной длины,
+    // и количество слов одинаковой длины (максимально одинаковой).
+    //
+    //“What a wonderful day!” -> [“a”, “wonderful”, “0”]
+    //“I love to learn Java!” -> [“I”, “learn”, “2”]
+
+
+    //4.Написать метод, который принимает на вход строку букв,  и  возвращает строку, в которой только уникальные буквы
+    //
+    //Пример:
+    //method(“abcaczdbdakcazdbaz”)
+    // Вернуть
+    //“abczdk"
+
+
+    //5.Написать метод, который проверяет строку на уникальность символов в ней.
+
+
+    //6.Даны две строки. Определите, можно ли из некоторых символов первой строки составить вторую строку.
+    //
+    //      Пример:
+    //method( “QAForEveryone”, “Verona”) -> true
+    //method( “QAForEveryone”, “frog”) -> false
+
+
+    //7.Write a function to find the longest common prefix string amongst an array of strings.
+    // If there is no common prefix, return an empty string "".
+    //Example 1:
+    //Input: String[] strs = {"flower","flow","flight"}
+    //Output: "fl"
+    //Example 2:
+    //Input: String[] strs = {"dog","racecar","car"}
+    //Output: ""
+    //Explanation: There is no common prefix among the input strings.
+
+
+    //8.Find the non-repeating length of the longest substring in a string
+    //Example :  aabcb                     Output expected: 3
+    //Example2: aaaaa        Output expected: 1
+
+
+    //9.Write a method that will take one string as an argument and will return the reverse version of this string.
+    //
+    //reverseStr("apple")  -> “elppa”
+    //10.Write a method that will take a string as an argument. The method will return a palindrome.
+    // A palindrome is a word, phrase, number, or sequence of words that reads the same backward as forward.
+    //
+    ////Такие варианты редко дают на интервью
+    //a)
+    //method(“А роза упала на лапу Азора”) -> “арозА упал ан алапу азор А”      или  “а розА упала на лапу азорА”
+    //
+    //
+    ////Этот вариант чаще дают на интервью
+    //b) Написать метод, который проверяет, является ли слово палиндромом.
+    //
+    //method(“Madam”) -> true    это палиндром
+    //method(12321) -> true
+
+
+    //11.Write a method, which checks if two given strings are Anagram of each other. Two String is said to be an
+    // anagram of each other, if they contain exactly the same characters but in a different order.
+    //
+    //Пример:
+    //method("O, Draconian devil!", "!Leonardo Da Vinci,")  -> true
+    //method("Oh, lame saint!", "The Mona Lisa,!") -> true
+
+
+    //12.Написать метод, который принимает на вход строку и производит вычисления в этой строке в порядке
+    // математических действий.
+    //Входные данные:
+    //"5*3-9/3"
+    //
+    //Выходные данные:
+    //“12"
+
+
+    //13.Строка состоит из слов, разделенных пробелами. Переставьте слова a) по убыванию их длин
+    //b) Отсортировать по алфавиту.
+
+
+    //14.Написать метод, который генерирует строку случайных букв русского алфавита.
+
+
+    //15.Напсиать метод, который генерирует строку слов случайной длины, состоящую из случайных букв
+    // английского алфавита.
+}

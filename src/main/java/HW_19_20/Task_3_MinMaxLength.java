5baea234be7a597bef507876b479336ce0757193
Anastasiia Potapenko <oberonapa@gmail.como>
2021-11-05 20:09:31 -0400

    correcting

--- src/main/java/HW_19_20/Task_3_MinMaxLength.java
+++ src/main/java/HW_19_20/Task_3_MinMaxLength.java
@@ -7,45 +7,50 @@ public class Task_3_MinMaxLength {
     // и количество слов одинаковой длины (максимально одинаковой).
     //
     //“What a wonderful day!” -> [“a”, “wonderful”, “0”]
-    //“I love to learn Java mava!” -> [“I”, “learn”, “3”]
+    //“I love to to to learn Java mava!” -> [“I”, “learn”, “3”]
 
     public static void main(String[] args) {
-        getMinMaxLetter("I love to learn Java mava!");
+        getMinMaxLetter("I love to to to learn Java mava!");
     }
 
-    public static String[] getMinMaxLetter(String str){
+    public static String[] getMinMaxLetter(String str) {
 
         String[] words = str.split("\\s+");
         String min = "";
         String max = "";
-        int count=1;
+        int count = 0;
         int maxLength = Integer.MIN_VALUE;
         int minLength = Integer.MAX_VALUE;
-
         for (int i = 0; i < words.length; i++) {
             words[i] = words[i].replaceAll("[^\\w]", "");
         }
-
         for (int i = 0; i < words.length; i++) {
             for (int j = i + 1; j < words.length; j++) {
-                if((words[i].length())==((words[j].length()))){
+                if (words[i].length() < minLength) {
+                    minLength = words[i].length();
+                    min = words[i];
+                }
+                if (words[i].length() > maxLength) {
+                    maxLength = words[i].length();
+                    max = words[i];
+                }
+            }
+        }
+        int matches=0;
+        for (int i = 0; i < words.length; i++) {
+            if (matches<count && count>1) {
+                matches = count;
+            }
+            count = 0;
+            maxLength -= i;
+            for (int j = 0; j < words.length; j++) {
+                if (maxLength == words[j].length()) {
                     count++;
                 }
             }
-            if(words[i].length()<minLength){
-                minLength=words[i].length();
-                min=words[i];
-            }
-            if(words[i].length()>maxLength){
-                maxLength=words[i].length();
-                max=words[i];
-            }
-        }
-        if(count==1){
-            count=0;
         }
 
-        System.out.println(Arrays.toString(new String[]{min, max, String.valueOf(count)}));
-        return new String[]{min,max,String.valueOf(count)};
+        System.out.println(Arrays.toString(new String[]{min, max, String.valueOf(matches)}));
+        return new String[]{min, max, String.valueOf(matches)};
     }
 }

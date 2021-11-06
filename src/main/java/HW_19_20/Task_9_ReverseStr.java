c05c1b99a3e9a240f220f8bf62fc3ec91aee248a
Anastasiia Potapenko <oberonapa@gmail.como>
2021-11-06 11:04:50 -0400

    Task 9 Reverce

---
+++ src/main/java/HW_19_20/Task_9_ReverseStr.java
@@ -0,0 +1,13 @@
+package HW_19_20;
+
+public class Task_9_ReverseStr {
+    //Write a method that will take one string as an argument and will return the reverse version of this string.
+    public static String getReverseStr(String str){
+        StringBuilder reverseStr = new StringBuilder();
+        for (int i = str.length()-1; i >= 0; i--) {
+            reverseStr.append(str.charAt(i));
+        }
+        System.out.println(reverseStr.toString());
+        return reverseStr.toString();
+    }
+}
